a17b4dfc64d2ce81cdf709875ea10fc008d70e96
AngelMelecio <joseangelsm50@gmail.com>
2023-01-02 11:24:04 -0600

    api/login agregada en App Empleados

--- backend/Empleados/urls.py
+++ backend/Empleados/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from Empleados.api import empleado_api_view,empleado_detail_api_view
+from Empleados.api import empleado_api_view,empleado_detail_api_view, login
 
 urlpatterns = [
     path('empleados/', empleado_api_view, name='empleados_api'),
-    path('empleados/<int:pk>', empleado_detail_api_view, name='empleado_detail_api_view')
+    path('empleados/<int:pk>', empleado_detail_api_view, name='empleado_detail_api_view'),
+    path('login/', login, name='login_api')
 ]
